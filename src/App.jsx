57332f9d498a1f58be645b84b00d8889e33b012a
Ben Hogle <ben.hogle32@gmail.com>
2025-03-16 15:48:44 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useState } from "react";
+import { puppyList } from "./data.js";
 import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  console.log(puppyList);
+  //Hook useState creates special variable and special function
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPupInfo = puppies.find((puppy) => puppy.id === featPupId);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h2 className="puppy-heading">Puppy Pals</h2>
+      {featPupId && (
+        <div>
+          <h2>Info for for Puppy with ID: {featPupId}</h2>
+          <h2>{featuredPupInfo.name}</h2>
+          <ul>
+            <li>Age: {featuredPupInfo.age}</li>
+            <li>Email: {featuredPupInfo.email}</li>
+          </ul>
+        </div>
+      )}
+      {puppies.map((puppy) => (
+        <p
+          className="body-text"
+          onClick={() => {
+            setFeatPupId(puppy.id);
+          }}
+          key={puppy.id}
+        >
+          {puppy.name}
+        </p>
+      ))}
     </>
   );
 }
-
-export default App;
